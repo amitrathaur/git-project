28110db16ace40ad36a9596df4511eae2294bea0
Amit <amitrathauraxis333@gmail.com>
2026-06-23 12:35:08 +0530

    Added

--- my_next_file1.php
+++ my_next_file1.php
@@ -1,8 +1,3 @@
 <?php
 echo "Hello, Worldfffff!";
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 30b3cbe8bb84df6574f37c7bf90cf3215a47c1c5
-echo "New file created to add in git hub testing";
+echo "New file created to add in git hub testing (Testing)";
